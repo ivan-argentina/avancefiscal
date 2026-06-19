bfb2023dcbfa394777655d5f23e4eac7c203632e
Ivan Genari <ivan.genari@garciahnos.com.ar>
2026-06-19 16:47:49 -0300

    Agregar endpoint ping

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,11 @@ app.get("/ping", (req, res) => {
     mensaje: "backend funcionando",
   });
 });
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend en puerto ${PORT}`);
+});
 
 app.get("/api/test", async (req, res) => {
   try {
